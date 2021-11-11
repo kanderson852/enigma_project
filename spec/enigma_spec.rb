2d2a840e886b0e9e53db4eb0a265be925bc76d9d
Kelly Anderson <kean9528@colorado.edu>
2021-11-10 18:58:29 -0700

    make a stub for date

--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,18 +1,23 @@
+require 'spec_helper'
 require 'date'
-require "rspec"
 require "./lib/enigma"
-
+require './lib/encrypter'
 describe Enigma do
   let(:enigma) {Enigma.new}
   it 'exists' do
     expect(enigma).to be_an_instance_of(Enigma)
   end
 
-  it 'can encrypt' do
+  xit 'can encrypt' do
     expect(enigma.encrypt(message, key, date)).to be_a(Hash)
   end
 
-  it 'can decrypt' do
+  xit 'can decrypt' do
     expect(enigma.decrypt(message, key, date)).to be_a(Hash)
   end
+
+  it 'uses current date as a default' do
+    allow(Date).to receive(:today).and_return(Date.new(2001,2,3))
+  end
+
 end
