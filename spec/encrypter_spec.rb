bf5c8f627e9400f4d930301bb7088992e4e08472
Kelly Anderson <kean9528@colorado.edu>
2021-11-11 12:03:17 -0700

    make tests for keys module

--- spec/encrypter_spec.rb
+++ spec/encrypter_spec.rb
@@ -7,6 +7,7 @@ describe Encrypter do
 
   let(:encrypter) {Encrypter.new}
   let(:date) {Date.new(2001,2,3)}
+  let(:key) {allow(encrypter).to receive(:key_finder).and_return('01234')}
 
   it 'exists' do
     expect(encrypter).to be_an_instance_of(Encrypter)
@@ -45,5 +46,32 @@ describe Encrypter do
   it 'lists the offsets' do
     expect(encrypter.offset).to be_a(Hash)
     expect(encrypter.offset(date)).to eq( {'A' => 0, 'B' => 4, 'C' => 0, 'D' => 1})
+  end
+
+  it 'finds keys' do
+    expect(encrypter.key_finder).to be_a(String)
+    expect(encrypter.key_finder.length).to eq(5)
+    allow(encrypter).to receive(:key_finder).and_return('01234')
+    expect(encrypter.key_finder).to eq('01234')
+  end
+
+  it 'a_key' do
+    allow(encrypter).to receive(:key_finder).and_return('01234')
+    expect(encrypter.key_finder).to eq('01')
+  end
+
+  it 'b_key' do
+    allow(encrypter).to receive(:key_finder).and_return('01234')
+    expect(encrypter.key_finder).to eq('12')
+  end
+
+  it 'c_key' do
+    allow(encrypter).to receive(:key_finder).and_return('01234')
+    expect(encrypter.key_finder).to eq('23')
+  end
+
+  it 'd_key' do
+    allow(encrypter).to receive(:key_finder).and_return('01234')
+    expect(encrypter.key_finder).to eq('34')
   end
 end
