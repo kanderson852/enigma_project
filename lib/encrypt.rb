b3b84aec3f338ee4170252e3de4f451044f6898f
Kelly Anderson <kean9528@colorado.edu>
2021-11-13 15:18:23 -0700

    practice with text files

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,2 +1,13 @@
-file = File.open("message.txt")
-message = file.read
+message = File.open("message.txt", 'r')
+encrypted = File.open('encrypted.txt', "w")
+message.read
+message.rewind
+a = message.readlines
+encrypted.write(a)
+encrypted.close
+message.close
+# require "pry"; binding.pry
+ARGV == ["message.txt", "encrypted.txt"]
+ARGV[0] == "message.txt"
+ARGV[1] == "encrypted.txt"
+puts "Created 'encrypted.txt' with the key 82648 and date 240818"
