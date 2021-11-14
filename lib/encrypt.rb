affc95e08e472ae4931428a5f8e4387fbccbedbf
Kelly Anderson <kean9528@colorado.edu>
2021-11-14 11:00:17 -0700

    make runner files work

--- lib/encrypt.rb
+++ lib/encrypt.rb
@@ -1,13 +1,11 @@
-message = File.open("message.txt", 'r')
-encrypted = File.open('encrypted.txt', "w")
-message.read
-message.rewind
-a = message.readlines
-encrypted.write(a)
-encrypted.close
-message.close
-# require "pry"; binding.pry
-ARGV == ["message.txt", "encrypted.txt"]
-ARGV[0] == "message.txt"
-ARGV[1] == "encrypted.txt"
-puts "Created 'encrypted.txt' with the key 82648 and date 240818"
+require './enigma'
+enigma = Enigma.new
+input = ARGV[0]
+output = ARGV[1]
+key = '1234'
+date = '111421'
+encryption_data = enigma.encrypt(File.read(input).tr("\n", ""), key, date)
+File.open(output, "w") do |file|
+  file.puts encryption_data[:encryption]
+  puts "Created #{output} with the key #{encryption_data[:key]} and date #{encryption_data[:date]}"
+end
